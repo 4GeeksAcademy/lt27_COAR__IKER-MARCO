1da5d6ffbaf30cb17d28bf6c1a094dd2167efb0e
MarckDmarck <155605126+MarckDmarck@users.noreply.github.com>
2024-05-11 01:20:03 +0000

    Se agrego el modelo Category

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Category
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 
@@ -20,3 +20,24 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/category', methods=['GET'])
+def all_category():
+    category = Category.query.all()
+    resp = list(map(lambda element: element.serialize(),category))
+
+    if resp == None:
+        response_body = {
+            "msg":"no categories"
+        }
+        return jsonify(response_body),404
+    else:
+        resp
+        return jsonify(resp),200
+    
+
+# @api.route('/category/new', methods=['POST'])
+# def new_category():
+    
+
+#     return jsonify(response_body), 200
