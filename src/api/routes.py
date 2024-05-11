a151221a08cb5a1d92fbc5fb832cb9912ad4ef1c
iluna007 <146373472+iluna007@users.noreply.github.com>
2024-05-11 02:33:37 +0000

    agregue CRUD CRAFTMAN

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Craftmen
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 
@@ -20,3 +20,53 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+#################################CRUD CRAFTMEN#################################
+@api.route('/craftmen', methods=['GET'])
+def get_craftmen():
+    craftmen = Craftmen.query.all()
+    craftmen = list(map(lambda x: x.serialize(), craftmen))
+
+    return jsonify(craftmen), 200
+
+@api.route('/craftmen/<int:id>', methods=['GET'])
+def get_craftmen_by_id(id):
+    craftmen = Craftmen.query.get(id)
+    craftmen = craftmen.serialize()
+
+    return jsonify(craftmen), 200
+
+@api.route('/craftmen', methods=['POST'])
+def create_craftmen():
+    request_body = request.get_json()
+    craftmen = Craftmen(name=request_body["name"], last_name=request_body["last_name"], email=request_body["email"], password=request_body["password"], phone=request_body["phone"], address=request_body["address"], city=request_body["city"], state=request_body["state"], zip_code=request_body["zip_code"], is_active=request_body["is_active"])
+    db.session.add(craftmen)
+    db.session.commit()
+    return jsonify("Craftmen created"), 200
+
+@api.route('/craftmen/<int:id>', methods=['PUT'])
+def update_craftmen(id):
+    craftmen = Craftmen.query.get(id)
+    request_body = request.get_json()
+    craftmen.name = request_body["name"]
+    craftmen.last_name = request_body["last_name"]
+    craftmen.email = request_body["email"]
+    craftmen.password = request_body["password"]
+    craftmen.phone = request_body["phone"]
+    craftmen.address = request_body["address"]
+    craftmen.city = request_body["city"]
+    craftmen.state = request_body["state"]
+    craftmen.zip_code = request_body["zip_code"]
+    craftmen.is_active = request_body["is_active"]
+    db.session.commit()
+    return jsonify("Craftmen updated"), 200
+
+@api.route('/craftmen/<int:id>', methods=['DELETE'])
+def delete_craftmen(id):
+    craftmen = Craftmen.query.get(id)
+    db.session.delete(craftmen)
+    db.session.commit()
+    return jsonify("Craftmen deleted"), 200
+
+################################---------#############################
