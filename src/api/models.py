055f5b5a926fbbb299b10204bf0671038afad9b7
MarckDmarck <155605126+MarckDmarck@users.noreply.github.com>
2024-05-22 04:41:43 +0000

    Se agrego el CRUD Buyer

--- src/api/models.py
+++ src/api/models.py
@@ -112,3 +112,25 @@ class Admiin(db.Model):
             "email": self.email
         }
 
+#################### MODEL BUYER ##################################################
+
+class Buyer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    lastName = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    address = db.Column(db.String(120), unique=False, nullable=False)
+   
+    def __repr__(self):
+        return f'<Buyer {self.name}>'
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "lastName": self.lastName,
+            "email": self.email,
+            "address": self.address
+        }
+
