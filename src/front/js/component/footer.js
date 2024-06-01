ff729d29ec3328897859eb52d2c68a79071a5681
iluna007 <146373472+iluna007@users.noreply.github.com>
2024-06-01 01:10:34 +0000

    agregue mas front

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -2,9 +2,66 @@ import React, { Component } from "react";
 
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
+		<div className="py-5 py-lg-6 bg-gray-100" bg-secondary>
+			<div className="container">
+				<div className="row">
+					<div className="py-4 service-column col-lg-3 col-sm-6">
+						<i className="bi bi-truck"></i>
+						<div className="service-text">
+							<h6 className="text-sm mb-1">Free shipping; return</h6>
+							<p className="text-muted fw-light text-sm mb-0">Free Shipping over $300</p>
+						</div>
+					</div>
+					<div className="py-4 service-column col-lg-3 col-sm-6">
+						<i className="bi bi-piggy-bank"></i>
+						<div className="service-text">
+							<h6 className="text-sm mb-1">Money back guarantee</h6>
+							<p className="text-muted fw-light text-sm mb-0">30 Days Money Back Guarantee</p>
+						</div>
+					</div>
+					<div className="py-4 service-column col-lg-3 col-sm-6">
+						<i className="bi bi-tag"></i>
+						<div className="service-text">
+							<h6 className="text-sm mb-1">Best prices</h6>
+							<p className="text-muted fw-light text-sm mb-0">Always the best prices</p>
+						</div>
+					</div>
+					<div className="py-4 service-column col-lg-3 col-sm-6">
+						<i className="bi bi-headset"></i>
+						<div className="service-text">
+							<h6 className="text-sm mb-1">020-800-456-747</h6>
+							<p className="text-muted fw-light text-sm mb-0">24/7 Available Support</p>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+
+		<div className="py-4 fw-light bg-gray-800 text-gray-300">
+			<div className="container">
+				<div className="align-items-center row">
+					<div className="text-center text-md-start col-md-6">
+						<p className="text-sm mb-md-0">© 2021, Your company. All rights reserved.</p>
+					</div>
+					<div className="col-md-6">
+						<ul className="list-inline mb-0 mt-2 mt-md-0 text-center text-md-end">
+							<li className="list-inline-item">
+								<img width="32" height="32" src="/svg/visa.svg" alt="..." className="w-2rem" />
+							</li>
+							<li className="list-inline-item">
+								<img src="/svg/mastercard.svg" width="32" height="32" alt="..." className="w-2rem" />
+							</li>
+							<li className="list-inline-item">
+								<img src="/svg/paypal.svg" width="32" height="32" alt="..." className="w-2rem" />
+							</li>
+							<li className="list-inline-item">
+								<img src="/svg/western-union.svg" width="32" height="32" alt="..." className="w-2rem" />
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
 	</footer>
 );
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -3,6 +3,9 @@ import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
 
 import ProductCard_Buyer from "../component/productCard_Buyer";
+import { Carousel } from "../component/carousel.js";
+import { Jumbo } from "../component/jumbo.js";
+import { Cate } from "../component/cate.js";
 
 export const Home = () => {
   const { store, actions } = useContext(Context);
@@ -29,24 +32,12 @@ export const Home = () => {
   return (
     <div className="container mt-5">
       <div className="row text-center">
-        <div className="p-5 mb-4 bg-body-tertiary rounded-3">
-          <div className="container-fluid py-5">
-            <h1 className="display-5 fw-bold">Co-Ar</h1>
-            <p className="col-md-8 fs-4">
-              Welcome to Co-Ar, the place where you can find the best products
-              made by the best craftmen.
-            </p>
-            <Link to="/login">
-              <button type="button" className="btn btn-primary btn-lg">
-                Login
-              </button>
-            </Link>
-          </div>
-        </div>
+        <Jumbo />
         <h2>Featured Categories</h2>
-        
+        <Cate />
+
         <h2>Popular products</h2>
-        <div className="row mb-3 text-center col-md-10">
+        <div className="row flex-row flex-nowrap " style={{ overflowX: "auto" }}>
           {store.product.map((product) => {
             return (
               <ProductCard_Buyer
@@ -59,26 +50,10 @@ export const Home = () => {
               />
             );
           })}
-          </div>
+        </div>
+
         <h2>Best Sellers</h2>
-
-
-        <div className="card col-6 col-md-2">
-          <h2>Order by : </h2>
-          <select
-            className="form-select"
-            aria-label="Default select example"
-            name="category"
-            onChange={info}>
-            <option value="1">Category</option>
-            <option value="2">Price</option>
-            <option value="3">Stock</option>
-            <option value="4">Craftmen</option>
-          </select>
-
-        </div>
-
-        <div className="row mb-3 text-center col-md-10">
+        <div className="row flex-row flex-nowrap " style={{ overflowX: "auto" }}>
           {store.product.map((product) => {
             return (
               <ProductCard_Buyer
@@ -92,6 +67,7 @@ export const Home = () => {
             );
           })}
         </div>
+
       </div>
     </div>
   );
